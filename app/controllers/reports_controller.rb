5b844248156aedb6d8f57946d05bc4a4cc2464f9
takurooon <takurokodama0726@gmail.com>
2019-09-15 19:08:51 +0900

    i18n add devise.ja.yml model.ja.yml

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,4 +1,24 @@
 class ReportsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :edit, :update, :destroy]
+
   def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
   end
 end
