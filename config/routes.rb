1bf545b64d9b7a02b7947be647fcb444d2743d81
takurooon <takurokodama0726@gmail.com>
2019-09-15 15:50:48 +0900

    add gem devise/letter_opener, add reports controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'reports#index'
+
+  resources :reports
+  
+  get 'reports/index'
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
 end
