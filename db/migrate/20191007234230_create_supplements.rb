408b77117892822334bf8d96d804438cd92a4994
takurooon <takurokodama0726@gmail.com>
2019-10-08 08:43:39 +0900

    generate Supplement model

---
+++ db/migrate/20191007234230_create_supplements.rb
@@ -0,0 +1,9 @@
+class CreateSupplements < ActiveRecord::Migration[6.0]
+  def change
+    create_table :supplements do |t|
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_07_233831) do
+ActiveRecord::Schema.define(version: 2019_10_07_234230) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -86,6 +86,12 @@ ActiveRecord::Schema.define(version: 2019_10_07_233831) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_reports_on_user_id"
+  end
+
+  create_table "supplements", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
