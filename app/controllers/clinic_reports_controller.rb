a55c331e407a81485fcfae9d1f21802e0f52b86d
takurooon <takurokodama0726@gmail.com>
2019-10-10 22:07:03 +0900

    add clinic_report#new collection box bootstrap

--- app/controllers/clinic_reports_controller.rb
+++ app/controllers/clinic_reports_controller.rb
@@ -13,12 +13,14 @@ class ClinicReportsController < ApplicationController
   def create
     clinic_report = ClinicReport.new(clinic_report_params)
     clinic_report.report_id = params[:report_id].to_i
+    clinic_report.clinic_id = 1
     if clinic_report.save
       flash[:notice] = "レポートを作成しました"
       redirect_to clinic_report_path(clinic_report)
     else
       flash[:clinic_report] = clinic_report
       flash[:error_messages] = clinic_report.errors.full_messages
+      logger.error clinic_report.errors.full_messages
       redirect_back fallback_location: clinic_report
     end
   end
@@ -28,7 +30,7 @@ class ClinicReportsController < ApplicationController
 
   private
   
-  def clinic_report_params
-    params.require(:clinic_report).permit(:number_of_clinics, :current_state, :work_style, :address_at_that_time, :number_of_aih, :amh, :bmi, :types_of_eggs_and_sperm, :total_number_of_sairan, :number_of_eggs_collected, :total_number_of_transplants, :number_of_eggs_stored, :type_of_sairan_cycle, :types_of_fertilization_methods, :number_of_fertilized_eggs, :number_of_frozen_eggs, :cost, :credit_card_validity, :average_waiting_time, :clinic_selection_criteria, :content, :clinic_review, supplement_ids: [], female_infertility_factor_ids: [], male_infertility_factor_ids: [], female_disease_ids: [], male_disease_ids: [], female_surgery_ids: [], male_surgery_ids: [], ranso_shigeki_ids: [], hairan_yokusei_ids: [], hairan_yuhatsu_ids: [], isyoku_option_ids: [])
-  end
+    def clinic_report_params
+      params.require(:clinic_report).permit(:number_of_clinics, :current_state, :work_style, :address_at_that_time, :number_of_aih, :amh, :bmi, :types_of_eggs_and_sperm, :total_number_of_sairan, :number_of_eggs_collected, :total_number_of_transplants, :number_of_eggs_stored, :type_of_sairan_cycle, :types_of_fertilization_methods, :number_of_fertilized_eggs, :number_of_frozen_eggs, :cost, :credit_card_validity, :average_waiting_time, :clinic_selection_criteria, :content, :clinic_review, supplement_ids: [], female_infertility_factor_ids: [], male_infertility_factor_ids: [], female_disease_ids: [], male_disease_ids: [], female_surgery_ids: [], male_surgery_ids: [], ranso_shigeki_ids: [], hairan_yokusei_ids: [], hairan_yuhatsu_ids: [], isyoku_option_ids: [])
+    end
 end
